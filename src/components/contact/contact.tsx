ef04d6875ab00f4d75cd85b772c20a636cf71fd0
Alex Gorodov <a.gorodov.22@gmail.com>
2024-04-07 05:40:16 +0300

    form fixed

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -21,26 +21,33 @@ export function Contact(): JSX.Element {
 
   const sendForm = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
+
+    // Проверяем, есть ли ошибки в полях name или email
+    if (formData.name === '' || formData.email === '') {
+      setError(true);
+      return;
+    }
+
     setSending(true);
 
     try {
       await axios.post('https://formspree.io/f/mgebwvrj', formData);
+      setSuccess(true);
       setTimeout(() => {
+        setSuccess(false);
         setSending(false);
-        setSuccess(true);
         setFormData({
           name: '',
           email: '',
           subject: '',
           message: '',
         });
-        setTimeout(() => {
-          setSuccess(false);
-        }, 2000);
+        setError(false);
       }, 2000);
     } catch (error) {
       setError(true);
       console.error(error);
+      setSending(false);
     }
   };
 
@@ -51,71 +58,77 @@ export function Contact(): JSX.Element {
         <p className="description contact__description">
           I'm interested in freelance opportunities - landing pages and small sites, for now. However, if you have other request of question, don't hesitate to use the form.
         </p>
-        <div className="contact__form-wrapper">
-          {isSending ? (
-            <div style={{paddingTop: '60px'}}>
-              <Spinner size={'40'} color={'#000c24'} />
-            </div>
-          ) : isSuccess ? (
-            <p className="contact__success-message">Successfully sent!</p>
-          ) : (
-            <form className="contact__form" action="https://formspree.io/f/mgebwvrj" method="POST">
-              <label className="contact__item contact__item--name contact__item--required" htmlFor="name">
-                <input
-                  className={`contact__field ${isError && formData.name.length < 2 && 'contact__field--error'}`}
-                  type="text"
-                  name="name"
-                  id="name"
-                  placeholder="Name"
-                  minLength={2}
-                  required
-                  value={formData.name}
-                  onChange={handleInputChange}
-                />
-              </label>
-              <label className="contact__item contact__item--email contact__item--required" htmlFor="email">
-                <input
-                  className={`contact__field ${isError && formData.email.length < 5 && 'contact__field--error'}`}
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Email"
-                  minLength={5}
-                  required
-                  value={formData.email}
-                  onChange={handleInputChange}
-                />
-              </label>
-              <label className="contact__item contact__item--subject" htmlFor="subject">
-                <input
-                  className="contact__field"
-                  type="text"
-                  name="subject"
-                  id="subject"
-                  placeholder="Subject"
-                  value={formData.subject}
-                  onChange={handleInputChange}
-                />
-              </label>
-              <label className="contact__item contact__item--message contact__item--required" htmlFor="message">
-                <textarea
-                  className="contact__field contact__field--message"
-                  name="message"
-                  id="message"
-                  cols={30}
-                  rows={5}
-                  placeholder="Message"
-                  required
-                  value={formData.message}
-                  onChange={handleInputChange}
-                ></textarea>
-              </label>
-              <button className="contact__submit" type="submit" onClick={sendForm}>
-                Send message!
-              </button>
-            </form>
-          )}
-        </div>
+        {!isSuccess && (
+          <div className="contact__form-wrapper">
+            {isSending ? (
+              <div style={{paddingTop: '60px'}}>
+                <Spinner size={'40'} color={'#000c24'} />
+              </div>
+            ) : (
+              <form className="contact__form" action="https://formspree.io/f/mgebwvrj" method="POST">
+                <label className="contact__item contact__item--name contact__item--required" htmlFor="name">
+                  <input
+                    className={`contact__field ${isError && formData.name.length < 2 && 'contact__field--error'}`}
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Name"
+                    minLength={2}
+                    required
+                    value={formData.name}
+                    onChange={handleInputChange}
+                  />
+                </label>
+                <label className="contact__item contact__item--email contact__item--required" htmlFor="email">
+                  <input
+                    className={`contact__field ${isError && formData.email.length < 5 && 'contact__field--error'}`}
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email"
+                    minLength={5}
+                    required
+                    value={formData.email}
+                    onChange={handleInputChange}
+                  />
+                </label>
+                <label className="contact__item contact__item--subject" htmlFor="subject">
+                  <input
+                    className="contact__field"
+                    type="text"
+                    name="subject"
+                    id="subject"
+                    placeholder="Subject"
+                    value={formData.subject}
+                    onChange={handleInputChange}
+                  />
+                </label>
+                <label className="contact__item contact__item--message contact__item--required" htmlFor="message">
+                  <textarea
+                    className="contact__field contact__field--message"
+                    name="message"
+                    id="message"
+                    cols={30}
+                    rows={5}
+                    placeholder="Message"
+                    required
+                    value={formData.message}
+                    onChange={handleInputChange}
+                  ></textarea>
+                </label>
+                {isError && (formData.name === '' || formData.email === '') && (
+                  <p className="contact__error-message">Please fill in the required fields.</p>
+                )}
+                <button className="contact__submit" type="submit" onClick={sendForm}>
+                  Send message!
+                </button>
+              </form>
+            )}
+          </div>
+        )}
+        {isSuccess && (
+          <p className="contact__success-message">Successfully sent!</p>
+        )}
       </div>
     </section>
   );
