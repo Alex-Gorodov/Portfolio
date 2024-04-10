d77d681e91bdf19162cfcfc58fb54d29f31a75cc
Alex Gorodov <a.gorodov.22@gmail.com>
2024-04-11 00:36:36 +0300

    about bg 2.0

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -4,55 +4,72 @@ import { useEffect, useRef, useState } from 'react';
 import Pdf from '../../alex_gorodov_cv.pdf';
 
 export function About(): JSX.Element {
-  // const parallaxLeftRef = useRef<HTMLDivElement>(null);
-  // const parallaxRightRef = useRef<HTMLDivElement>(null);
+  const parallaxLeftRef = useRef<HTMLDivElement>(null);
+  const parallaxRightRef = useRef<HTMLDivElement>(null);
   const aboutRef = useRef<HTMLDivElement>(null);
-  // const [isAboutSectionInView, setAboutSectionInView] = useState(false);
+  const [isAboutSectionInView, setAboutSectionInView] = useState(false);
 
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const scrollY = window.scrollY;
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 1170);
 
-  //     if (parallaxLeftRef.current && parallaxRightRef.current) {
-  //       const speedLeftAttr = parallaxLeftRef.current.getAttribute('data-speed');
-  //       const speedRightAttr = parallaxRightRef.current.getAttribute('data-speed');
+    useEffect(() => {
+      const handleResize = () => {
+        setIsDesktop(window.innerWidth >= 1170);
+      };
 
-  //       if (speedLeftAttr && speedRightAttr) {
-  //         const speedLeft = parseFloat(speedLeftAttr);
-  //         const speedRight = parseFloat(speedRightAttr);
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
-  //         const offsetYLeft = -scrollY * speedLeft;
-  //         const offsetYRight = scrollY * speedRight;
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
 
-  //         parallaxLeftRef.current.style.transform = `translateY(${offsetYLeft / 2}px) rotate(${offsetYLeft / 8}deg)`;
-  //         parallaxRightRef.current.style.transform = `translateY(${offsetYRight / 2}px) rotate(${offsetYRight / 8}deg)`;
+      if (parallaxLeftRef.current && parallaxRightRef.current) {
+        const speedLeftAttr = parallaxLeftRef.current.getAttribute('data-speed');
+        const speedRightAttr = parallaxRightRef.current.getAttribute('data-speed');
 
-  //         const aboutRect = aboutRef.current?.getBoundingClientRect();
-  //         const aboutTop = aboutRect?.top;
+        if (speedLeftAttr && speedRightAttr) {
+          const speedLeft = parseFloat(speedLeftAttr);
+          const speedRight = parseFloat(speedRightAttr);
 
-  //         const windowHeight = window.innerHeight;
+          const offsetYLeft = -scrollY * speedLeft;
+          const offsetYRight = scrollY * speedRight;
 
-  //         if (aboutTop && aboutTop < windowHeight) {
-  //           setAboutSectionInView(true)
-  //         }
-  //       }
-  //     }
-  //   };
+          parallaxLeftRef.current.style.transform = `translateY(${offsetYLeft / 2}px) rotate(${offsetYLeft / 8}deg)`;
+          parallaxRightRef.current.style.transform = `translateY(${offsetYRight / 2}px) rotate(${offsetYRight / 8}deg)`;
 
-  //   window.addEventListener('scroll', handleScroll);
+          const aboutRect = aboutRef.current?.getBoundingClientRect();
+          const aboutTop = aboutRect?.top;
 
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll);
-  //   };
-  // }, []);
+          const windowHeight = window.innerHeight;
+
+          if (aboutTop && aboutTop < windowHeight) {
+            setAboutSectionInView(true)
+          }
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
     <section className='section about' id="about" ref={aboutRef}>
-      {/* <div className={`about__parallax about__parallax--left ${isAboutSectionInView && 'design'}`} data-speed={0.5} ref={parallaxLeftRef}>
-        <ParallaxA/>
-      </div>
-      <div className={`about__parallax about__parallax--right ${isAboutSectionInView && 'nothing'}`} data-speed={0.8} ref={parallaxRightRef}>
-        <ParallaxB/>
-      </div> */}
+      {
+        !isDesktop &&
+        <div className={`about__parallax about__parallax--left ${isAboutSectionInView && 'design'}`} data-speed={0.5} ref={parallaxLeftRef}>
+          <ParallaxA/>
+        </div>
+      }
+      {
+        !isDesktop &&
+        <div className={`about__parallax about__parallax--right ${isAboutSectionInView && 'nothing'}`} data-speed={0.8} ref={parallaxRightRef}>
+          <ParallaxB/>
+        </div>
+      }
       <div>
         <h2>About</h2>
         <p className='about__text'>
